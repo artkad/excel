9cc34ffe6e6bc46fd18ed2489e48cd625b7d32ff
devart <kadkin.artem@gmail.com>
2020-05-28 17:14:25 +0500

    Finish base framework functional

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -7,22 +7,23 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 
-const filename = ext => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
+const filename = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`;
 
 const jsLoaders = () => {
-		const loaders = [
-			{
-				loader: 'babel-loader',
-				options: {
-					presets: ['@babel/preset-env']
-			}
-		}	
-	]
-	if(isDev) {
-		loaders.push('eslint-loader')
-	}
-	return loaders
-}
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: ['@babel/plugin-proposal-class-properties'],
+      },
+    },
+  ];
+  if (isDev) {
+    loaders.push('eslint-loader');
+  }
+  return loaders;
+};
 
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
