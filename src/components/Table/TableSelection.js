754f0d07f3118c33f4d61a398295d23f97382195
devart <kadkin.artem@gmail.com>
2020-07-19 16:42:28 +0500

    finish redux and table logic

--- src/components/Table/TableSelection.js
+++ src/components/Table/TableSelection.js
@@ -17,8 +17,16 @@ export class TableSelection {
     this.group = [];
   }
 
+  get selectedIds() {
+    return this.group.map(($el) => $el.id());
+  }
+
   selectGroup($group) {
     this.group = $group;
     this.group.forEach(($el) => $el.addClass(TableSelection.className));
+  }
+
+  applyStyle(style) {
+    this.group.forEach(($el) => $el.css(style));
   }
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -12,7 +12,7 @@ class Dom {
   }
 
   text(text) {
-    if (typeof text === 'string') {
+    if (typeof text !== 'undefined') {
       this.$el.textContent = text;
       return this;
     }
@@ -72,6 +72,14 @@ class Dom {
       this.$el.style[key] = styles[key];
     });
   }
+
+  getStyles(styles = []) {
+    return styles.reduce((res, s) => {
+      res[s] = this.$el.style[s];
+      return res;
+    }, {});
+  }
+
   addClass(className) {
     this.$el.classList.add(className);
     return this;
@@ -79,6 +87,14 @@ class Dom {
   focus() {
     this.$el.focus();
     return this;
+  }
+
+  attr(name, value) {
+    if (value) {
+      this.$el.setAttribute(name, value);
+      return this;
+    }
+    return this.$el.getAttribute(name);
   }
 
   removeClass(className) {
